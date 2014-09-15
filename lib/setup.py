ac166ca997eeafdbeb2311a9e7a6d6abd848da1d
Timothy McFadden <timothy.mcfadden@avagotech.com>
2014-09-15 15:53:15 -0600

    prep for pypi

--- lib/setup.py
+++ lib/setup.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
         description="Data obfuscator package",
         author="Timothy McFadden",
         url="https://github.com/mtik00/obfuscator",
+        download_url="https://github.com/mtik00/obfuscator/releases/tag/v{}".format(__version__),
         install_requires=[],
         packages=find_packages(),
         package_data={"obfuscator": ['.*']},
